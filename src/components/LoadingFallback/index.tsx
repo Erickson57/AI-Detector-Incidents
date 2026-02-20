0c3f0a88dfd7ddfc7eac51a1ac797dbfa92bb5f4
Andrey Shimagin <andrey.shimagin@onlyoffice.com>
2026-02-20 12:27:56 +0300

    adding lazy load component

---
+++ src/components/LoadingFallback/index.tsx
@@ -0,0 +1,5 @@
+export const LoadingFallback = () => (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', fontSize: '32px', color: '#cecece' }}>
+        Загрузка приложения...
+    </div>
+);
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react';
+import { lazy, StrictMode, Suspense } from 'react';
 import { createRoot } from 'react-dom/client';
 import { ConfigProvider } from 'antd';
 import { StyleProvider } from '@ant-design/cssinjs';
-import App from './App';
-import './styles.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { LoadingFallback } from './components/LoadingFallback';
+import './styles.css';
+
+const App = lazy(() => import('./App'));
 
 const queryClient = new QueryClient({
     defaultOptions: {
@@ -16,12 +18,15 @@ const queryClient = new QueryClient({
     },
 });
 
+
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <QueryClientProvider client={queryClient}>
             <StyleProvider layer>
                 <ConfigProvider theme={{ zeroRuntime: true }}>
-                    <App />
+                    <Suspense fallback={<LoadingFallback/>}>
+                        <App /> 
+                    </Suspense>
                 </ConfigProvider>
             </StyleProvider>
         </QueryClientProvider>
